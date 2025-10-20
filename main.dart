6488ee634cc0edd920552bd7c5808bc33c022e73
Tousifahmedxd1 <tousifahmedx1@gmail.com>
2025-10-20 14:25:05 +0500

    Initial commit with main.dart

---
+++ main.dart
@@ -0,0 +1,17 @@
+void main() {
+  int marks = 45;
+  double attendance = 80.0;
+
+  if (marks > 40 && attendance > 75) {
+    print("The student has Passed");
+  } else {
+    print("The student has Failed");
+  }
+
+
+
+
+
+
+  
+}
