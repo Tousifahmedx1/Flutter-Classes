493ae0a0e9bbb6d8a7b08989e6211225383be1ff
Tousifahmedxd1 <tousifahmedx1@gmail.com>
2025-11-01 09:22:37 +0500

    All Updated Assignment 3

--- assignment3.dart
+++ assignment3.dart
@@ -1,5 +1,5 @@
 import 'dart:ffi';
-
+import 'dart:io';
 void main() {
   List<String> names = ['Ali', 'Sara', 'John', 'Ayesha'];
   print('All names: $names');
@@ -42,4 +42,27 @@ void main() {
   List <String> userElgibility = [ 'Alice', 'Bob', 'Charlie','Eligible', 'Diana' ];
   userElgibility.removeWhere( (name) => name == 'Eligible' );
   print('"Eligible user": $userElgibility');
+
+  // SELF TASK LOGIN SYSTEM
+  
+  // step one we are creating a login system
+  Map <String, String> users = {
+    'tousif@gmail.com': 'tousif123',
+    'zaka@gmail.com': 'zaka123',
+    'raza@gmail.com': 'raza123',
+    'ahmed@yahoo.com': 'ahmed123',
+  }; 
+  // step 2 we are taking input from user email
+  stdout.write('Enter your email: ');
+  String? email = stdin.readLineSync();
+  // step 3 we are taking input from user password 
+  stdout.write('Enter your password: ');
+  String? password = stdin.readLineSync();
+  // step 4 we are checking if email and password are correct
+  if (users.containsKey(email) && users[email] == password) {
+    print('Login successful! Welcome, $email');
+  } else {
+    print('Invalid email or password. Please try again.');
+
+  }
 }
