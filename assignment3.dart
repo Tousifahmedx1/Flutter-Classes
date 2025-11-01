15e193fee072cb1cd633f3a172e31beb7bb49053
Tousifahmedxd1 <tousifahmedx1@gmail.com>
2025-11-01 08:49:48 +0500

    Updated Assignmet3

--- assignment3.dart
+++ assignment3.dart
@@ -9,4 +9,30 @@ void main() {
 
   List<dynamic> student = ['Tousif Ahmed','CLass 12', 'Numbers 101','A+', 95.5];
   print('Student Details: $student');
+
+  List<int> numbers = [10, 20, 30, 40, 50];
+  int smallest = numbers[0];
+  for (int number in numbers) {
+    if (number < smallest) {
+      smallest = number;
+    }
+  }
+  print(  'Smallest number: $smallest');
+  print(  'Largest number: ${numbers.reduce((a, b) => a > b ? a : b)}');
+
+  List <int> scores = [85, 92, 78, 90, 88];
+  int maxScore = scores[0];
+  for (int score in scores) { 
+    if (score > maxScore) {
+      maxScore = score;
+    }
+  } 
+
+  print('Highest score: $maxScore'); 
+
+  List <String> originallist = [ 'red', 'green', 'blue', 'yellow' ];
+  List <String> reversedlist = originallist.reversed.toList();
+  print('Original list: $originallist');
+  print('Reversed list: $reversedlist');
+  
 }
